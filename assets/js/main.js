d98bc7ce7e96988c9ccb07ff32c46281958db3c7
duaren <renaudduaren@gmail.com>
2021-11-26 14:02:06 +0100

    fix qualification switch active color

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,7 @@ tabs.forEach(tab =>{
         tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
-        tab,classList.add('qualification__active')
+        tab.classList.add('qualification__active')
     })
 
 })
