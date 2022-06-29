4dbcb6d83bd28874eb61a8d7072749dc7fe024b4
duaren <renaudduaren@gmail.com>
2022-06-29 17:53:05 +0100

    🐛 fix section modal display bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,7 +171,7 @@ sr.reveal(`.skills__close`, {delay: 1000})
 sr.reveal(`.qualification__container`)
 sr.reveal(`.edu`, {delay: 600 })
 sr.reveal(`.work`, {delay: 600 })
-sr.reveal(`.services__content`) 
+/* sr.reveal(`.services__content`)  */
 sr.reveal(`.project__container`)
 sr.reveal(`.contact__information`, {delay: 600, origin: 'left'})
 sr.reveal(`.contact__form`,{delay: 900} )
